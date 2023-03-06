12c58b478dedfcd09bd8f1f3ab3ad103af517111
Erik Enriquez <32780885+emenriquez@users.noreply.github.com>
2023-03-05 21:06:56 -0600

    Update StudentList.cpp

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,6 +1,6 @@
 	#include "StudentList.h"
 
-	// initialize the list. The list starts with no Students
+	// Define a constructor to initialize the list. The list starts with no Students
     StudentList::StudentList() {}
 
 	// return the number of students currently in the list
@@ -8,50 +8,63 @@
 		return -1;
 	}
 
-	//add a student to the front (head) of the list.
+	//add a Node with a student to the front (head) of the list.
 	void StudentList::addFront(Student s) {}
 
-	//add a student to the back (tail) of the list.
+	//add a Node with a student to the back (tail) of the list.
 	void StudentList::addBack(Student s) {}
 
-	//Print out each student in the list.
+	//Print out the names of each student in the list.
 	void StudentList::printList() {}
 
-	// Remove the student at the back (tail) of the list
-	// should not fail if list is empty
+	// Remove the Node with the student at the back (tail) of the list
+	// should not fail if list is empty! Print an error message if this occurs
+	// Don't forget that your head and tail pointers will be null pointers if the list is empty
 	void StudentList::popBack() {}
 
-	// Remove the student at the front (head) of the list
-	// should not fail if list is empty
+	// Remove the Node with the student at the front (head) of the list
+	// should not fail if list is empty! Print an error message if this occurs
+	// Don't forget that your head and tail pointers will be null pointers if the list is empty
 	void StudentList::popFront() {}
 
 	//insert a student at the position "index".
 	// for this list, count head as index 0
 	// if index is outside of current list range, 
 	// print a message and insert the student at the back of the list
+	// the previous Node at your target index should be moved forward. "For exampe, Node with student at index i, becomes index i+1" 
+	// Remember that you already have methods that can add students to the front or back of list if needed! Don't repeat this code.
 	void StudentList::insertStudent(Student s, int index) {}
 
 	//find the student with the given id number and return them
 	// if no student matches, print a message 
-	// and return a dummy student object
+	// and create and return a dummy student object
 	Student StudentList::retrieveStudent(int idNum) {
 		Student fixthis;
 		return fixthis;
 	}
 
-	// Remove a student from the list with a given id number
+	// Remove a Node with a student from the list with a given id number
 	// If no student matches, print a message and do nothing
 	void StudentList::removeStudentById(int idNum) {}
 
 	//Change the gpa of the student with given id number to newGPA
 	void StudentList::updateGPA(int idNum, float newGPA) {}
 
-	//Add all students from otherlist to this list.
+	//Add all students from otherList to this list.
 	//otherlist should be empty after this operation.
-	void StudentList::mergeList(StudentList &otherlist) {}
+	/*
+	For example, if the list has 3 students:
+	s1 <-> s2 <-> s3
+	and otherList has 2 students
+	s4 <-> s5
+	then after mergeList the currently list should have all 5 students
+	s1 <-> s2 <-> s3 <-> s4 <-> s5
+	and otherList should be empty and have zero students.
+	*/
+	void StudentList::mergeList(StudentList &otherList) {}
 
-	//create a list of students whose gpa is at least minGPA.
-	//Return this list.  The original list should
+	//create a StudentList of students whose gpa is at least minGPA.
+	//Return this list.  The original (current) list should
 	//not be modified (do not remove the students from the original list).
 	StudentList StudentList::honorRoll(float minGPA) {
 		StudentList fixthis;
